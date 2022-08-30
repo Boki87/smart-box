e5cb2d05bd8ec2af87bee21757fb61eda86fe8b3
Boki87 <peric.bojan87@gmail.com>
2022-08-30 23:08:05 +0200

    font size change

--- src/components/DeviceSocials.jsx
+++ src/components/DeviceSocials.jsx
@@ -16,7 +16,7 @@ const DeviceSocials = ({ socials }) => {
               />
             </Box>
             <Center>
-              <Text color="gray.700" fontSize="xl" textAlign="center">{social.title}</Text>
+              <Text color="gray.600" maxW="md" fontSize="m" textAlign="center">{social.title}</Text>
             </Center>
             </>
           );
